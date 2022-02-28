487b6738458c86a06f6433b6d5abded2d169f98d
allisonwang25 <87393358+allisonwang25@users.noreply.github.com>
2022-02-28 13:37:51 -0800

    Update RandomString.java

--- RandomString.java
+++ RandomString.java
@@ -2,7 +2,7 @@
 import java.util.*;
 public class RandomString
 {    
-  public static void main(String[] args)    
+  public static void run()    
   {        
     int i = (int)(Math.random() * 20);
     
