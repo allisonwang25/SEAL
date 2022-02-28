36c4e66758fbb6698958c87bf77d16d7f6f8e682
allisonwang25 <87393358+allisonwang25@users.noreply.github.com>
2022-02-27 23:44:11 -0800

    guess the word? black jack?

--- main.java
+++ main.java
@@ -7,27 +7,41 @@ public class main {
         Scanner pencil = new Scanner(System.in);
         System.out.println("hey there you procrastinator, you lazy human, you greasy sloth--what would you like to do to evade productivity?");
         while(true){
-            System.out.println("(1)Hangman : word wrong --> neck gone \n(2) pressTheP : press the 'p' and nothing else!\n(3) CardWar : we take no prisoners!\n(4) HighLow : guess strategically & hope... ;\n(5) ticTacToe : X's & O's \n(6) Slap Jack : slap that jackussy... \n(7) Typing Test : type your fingers off to quotes from your favorite TV shows\n(8) MysteryNumber : gUeSs ThAt nUmbEr! \n(9) Connect4 : all it takes to win is 1...2...3...4!");
+            System.out.println("(1) Hangman : word wrong --> neck gone \n" +
+                               "(2) pressTheP : press the 'p' and nothing else!\n" +
+                               "(3) CardWar : we take no prisoners!\n" +
+                               "(4) HighLow : guess strategically & hope... \n" +
+                               "(5) ticTacToe : X's & O's \n" +
+                               "(6) Slap Jack : slap that jackussy... \n" +
+                               "(7) Typing Test : type your fingers off to quotes from your favorite TV shows! \n"
+                               "(8) Mystery Number : guess in 5 tries or else!!! \n" + 
+                               "(9) Connect 4 : very self explanatory ;) \n" + 
+                               "(10)Guess the Word : \n" + 
+                               "(11) Black Jack : casino casinooo don't get addicted and don't count cards aaaa");
             int input = pencil.nextInt();
             pencil.nextLine();
             if(input==1)
                 Hangman.run();
-            if(input==2)
+            else if(input==2)
                 pressTheP.run();
-            if(input==3)
+            else if(input==3)
                 CardWar.run();
-            if(input==4)
+            else if(input==4)
                 HighLow.run();
-            if(input==5)
-                ticTacToe.run();
-            if(input==6)
+            else if(input==5)
+                tictactoe.run();
+            else if(input==6)
                 slapJack.run();
-            if(input==7)
+            else if(input==7)
                 typingTest.run();
-            if(input==8)
+            else if(input==8)
                 MysteryNumber.run();
-            if(input==9)
+            else if(input==9)
                 Connect4.run();
+            else if(input==10)
+                GuessTheWord.run();
+            //else if(input==11)
+                //blackJack.run();
             System.out.println("(1)quit or (2) play again?");
             input = pencil.nextInt();
             pencil.nextLine();
